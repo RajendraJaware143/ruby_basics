c79508579b879eccea2fe67f1c05a8f9f134f885
rajendra jaware <rajendrajaware143@gmail.com>
2018-02-14 17:46:25 +0530

    Array in reby

---
+++ array.rb
@@ -0,0 +1,14 @@
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]   
+#puts days[0]
+puts days.fetch(10,"Rajsaheb")   
+#puts days.push("Today")
+puts days.unshift("today")
+puts days<<("tomorrow")
+=begin 
+
+puts days[10]   
+puts days[-2]     
+puts days[2, 3]   
+puts days[1..7] 
+
+=end 
