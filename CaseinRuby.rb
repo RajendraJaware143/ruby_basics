2af5064a5174d475f8225d8950e1a9c156dd5acf
rajendra jaware <rajendrajaware143@gmail.com>
2018-02-14 17:39:19 +0530

    Case in ruby

---
+++ CaseinRuby.rb
@@ -0,0 +1,25 @@
+# Code for case in ruby
+
+print "Enter your day"
+day=gets.chomp.to_i
+case day
+when "Monday"
+	puts "The day is Tuesday"
+
+when "Tuesday"
+	puts "The day is Monday"
+
+when "Wed"
+	puts "The day is Monday"
+	
+when "Friday"
+	puts "The day is Monday"
+
+when "Saturday"
+	puts "The day is Monday"
+
+else
+	puts "The day is another day"
+
+
+end
